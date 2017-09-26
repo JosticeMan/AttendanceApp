044295f36ed8735f01d231cd5c6ef6526341a33d
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-09-26 11:41:41 -0400

    -

---
+++ Attendance Justin Yau/src/main/Student.java
@@ -0,0 +1,51 @@
+package main;
+
+public class Student implements Attendee {
+	
+	private String firstName;
+	private String lastName;
+	private boolean isPresent = false;
+	
+	public Student(String fn, String ln)
+	{
+		firstName = fn;
+		lastName = ln;
+	}
+	
+	public boolean isPresent() 
+	{
+		return isPresent;
+	}
+	
+	public void setPresent(boolean present)
+	{
+		isPresent = present;
+	}
+	
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	
+	public String getLastName()
+	{
+		return lastName;
+	}
+	
+	public boolean mathces(String first, String last)
+	{
+		return first.equalsIgnoreCase(firstName) && last.equalsIgnoreCase(lastName);
+	}
+	
+	public boolean matches(String last)
+	{
+		return last.equalsIgnoreCase(lastName);
+	}
+	
+	public String getReportString()
+	{
+		String output = "";
+		return output;
+	}
+	
+}
